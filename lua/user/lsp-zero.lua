45636aadbeb4b045c025f1e32c102dca0b5ad8db
Khanh Nguyen <nhkk49@gmail.com>
2022-12-21 09:42:00 -0500

    Using kickstart minimum setup with DAP

--- lua/user/lsp-zero.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, lsp_zero = pcall(require, "lsp-zero")
-if not status_ok then 
-  print ('lsp not working; in zero file')
-  return
-end
-
-lsp_zero.preset('recommended')
-lsp_zero.setup()
